a0fcdcbdc730b0a7acdad832e5ab6d0301486d26
falchizao <marcelonavarro11md@gmail.com>
2022-02-19 21:30:06 -0300

    controller updated

--- products/src/main/java/com/example/products/view/controller/ProductController.java
+++ products/src/main/java/com/example/products/view/controller/ProductController.java
@@ -7,16 +7,23 @@ import java.util.stream.Collectors;
 
 import com.example.products.services.ProductService;
 import com.example.products.shared.ProductDTO;
+import com.example.products.view.model.ProductRequest;
 import com.example.products.view.model.ProductResponse;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.function.EntityResponse;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/api/products")
@@ -40,5 +47,33 @@ public class ProductController {
 
         return new ResponseEntity<>(Optional.of(products) , HttpStatus.OK); //Status 200
     }
+
+
+    @PostMapping
+    public ResponseEntity<ProductResponse> postMethodName(@RequestBody ProductRequest product) {
+        //TODO: process POST request
+        ProductDTO dto = new ModelMapper().map(product, ProductDTO.class);
+        dto = productService.addProduct(dto);
+
+        return new ResponseEntity<>(new ModelMapper().map(dto, ProductResponse.class) , HttpStatus.CREATED);    
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteProduct(@PathVariable Integer id){
+        productService.deleteProduct(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductResponse> updateProduct(@PathVariable Integer id, @RequestBody ProductRequest product){
+        ProductDTO dto = new ModelMapper().map(product, ProductDTO.class);
+        dto.setId(id);
+        productService.addProduct(dto);
+
+        return new ResponseEntity<>(new ModelMapper().map(dto, ProductResponse.class) , HttpStatus.OK);
+    }
+    
     
 }
